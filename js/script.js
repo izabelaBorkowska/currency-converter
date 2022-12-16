3eb43f2cb471be3661e63dba05e1aadaeb724458
izabelaBorkowska <117465630+izabelaBorkowska@users.noreply.github.com>
2022-12-16 11:17:37 +0100

    functions added

--- js/script.js
+++ js/script.js
@@ -5,60 +5,36 @@
 
   welcome();
 
+  const calculateResult = (amount) => {
+    const eurElement = document.querySelector(".js-eur");
+    const gbpElement = document.querySelector(".js-gbp");
+    const sekElement = document.querySelector(".js-sek");
+    const usdElement = document.querySelector(".js-usd");
+    switch (true) {
+      case usdElement.checked:
+        return `${(amount / usdElement.value).toFixed(2)} USD`;
+      case eurElement.checked:
+        return `${(amount / eurElement.value).toFixed(2)} EUR`;
+      case gbpElement.checked:
+        return `${(amount / gbpElement.value).toFixed(2)} GBP`;
+      default:
+        return `${(amount / sekElement.value).toFixed(2)} SEK`;
+    }
+  };
 
-c 
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-const calculatResult = (true, pln) => {
-  const pln = +plnElement.value;
-  const usd = +usdElement.value;
-  const eur = +eurElement.value;
-  const gbp = +gbpElement.value;
-  const sek = +sekElement.value;
-  let name;
+    const plnElement = document.querySelector(".js-pln");
+    const resultElement = document.querySelector(".js-result");
 
+    resultElement.innerText = calculateResult(plnElement.value);
+  };
 
-  switch (true) {
-    case usdElement.checked:
-      console.log("usd");
-      return = pln / usd;
-      name = "USD";
-     case eurElement.checked:
-      return = pln / eur;
-      name = "EUR";
-     case gbpElement.checked:
-      return = pln / gbp;
-      name = "GBP";
-     default:
-      return = pln / sek;
-      name = "SEK";
-  }
-}
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+    formElement.addEventListener("submit", onFormSubmit);
+  };
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-
-  onst plnElement = document.querySelector(".js-pln");
-const eurElement = document.querySelector(".js-eur");
-const gbpElement = document.querySelector(".js-gbp");
-const sekElement = document.querySelector(".js-sek");
-const usdElement = document.querySelector(".js-usd");
-const formElement = document.querySelector(".form");
-const resultElement = document.querySelector(".js-result");
-const nameElement = document.querySelector(".js-name"); 
-
-
-  const pln = +plnElement.value;
-  const usd = +usdElement.value;
-  const eur = +eurElement.value;
-  const gbp = +gbpElement.value;
-  const sek = +sekElement.value;
-  const result = calculatResult(true, pln);
-  let name;
-
-
-  resultElement.innerText = result.toFixed(2);
-  nameElement.innerText = ` ${name}`;
-});
-
+  init();
 }
